7257628cb7e4bbae6b8f7f703fc739a0688f92e5
machadoluan <machado.luandealmeida@gmail.com>
2025-03-11 23:09:55 -0300

    fix: update database configuration to use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,25 +15,22 @@ import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    
     TypeOrmModule.forRoot({
       type: 'mysql',
-      // host: process.env.DB_HOST,
-      // port: parseInt(process.env.DB_PORT, 10),
-      // username: process.env.DB_USERNAME,
-      // password: process.env.DB_PASSWORD,
-      // database: process.env.DB_DATABASE,
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      database: 'newyork',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [User],
       synchronize: true
     }),
     AuthModule,
     AccountModule,
-    ConfigModule.forRoot({
-      isGlobal: true
-    }),
     TwilioModule,
     DiscordModule,
     PaymentModule,
