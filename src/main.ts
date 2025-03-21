3adbc5ca1fd65b529365f1e9ec1957e9bb0686ad
machadoluan <machado.luandealmeida@gmail.com>
2025-03-21 20:38:05 -0300

    feat: add cors na aplicação

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import * as passport from 'passport';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: 'https://groovegg.com.br',
+    credentials: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
