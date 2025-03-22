0b0af1aadbb19378ec3ab46ccfce63f5c6a0031f
machadoluan <machado.luandealmeida@gmail.com>
2025-03-22 00:23:57 -0300

    feat: adicionar suporte a CORS para localhost no arquivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://groovegg.com.br',
+    origin: ['https://groovegg.com.br', 'http://localhost:4200'],
     credentials: true,
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
